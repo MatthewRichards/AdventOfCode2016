7c3e8bb3fe46d0f1a4b08532b4ea365fca88586f
Matthew Richards <Matthew.Richards@softwire.com>
2016-12-05 17:18:56 +0000

    Day 3, part 1

---
+++ AdventOfCode2016/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2016
+{
+  static class StringExtensions
+  {
+    public static IEnumerable<string> SplitLines(this string input)
+    {
+      return input.Split(new[] {Environment.NewLine}, StringSplitOptions.None);
+    } 
+  }
+}
