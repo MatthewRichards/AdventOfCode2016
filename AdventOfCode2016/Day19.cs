afff6853720d4fd615b552be626e8e5dcd4c5487
Matthew Richards <Matthew.Richards@softwire.com>
2016-12-19 17:15:39 +0000

    Move day 19 back to just a simple boolean array

--- AdventOfCode2016/Day19.cs
+++ AdventOfCode2016/Day19.cs
@@ -15,6 +15,51 @@ namespace AdventOfCode2016
       timer.Start();
 
       int elfCount = 3017957;
+      bool[] elves = new bool[elfCount];
+      int elfIndex = 0;
+      int targetElfDistance = 0;
+      int targetElf = 0;
+      int remainingElves = elfCount;
+
+      while (remainingElves > 1)
+      {
+        int acrossTheCircle = remainingElves / 2;
+
+        for (int i = targetElfDistance; i < acrossTheCircle; i++)
+        {
+          do
+          {
+            targetElf = (targetElf + 1) % elfCount;
+          } while (elves[targetElf]);
+        }
+
+        elves[targetElf] = true;
+        remainingElves--;
+        targetElfDistance = acrossTheCircle - 1;
+
+        do
+        {
+          targetElf = (targetElf + 1)%elfCount;
+        } while (elves[targetElf]); 
+
+        do
+        {
+          elfIndex = (elfIndex + 1) % elfCount;
+        } while (elves[elfIndex]);
+      }
+
+      timer.Stop();
+      Console.WriteLine($"Final elf: {elfIndex + 1}");
+      Console.WriteLine($"Total time: {timer.ElapsedMilliseconds}ms");
+      Console.ReadKey();
+    }
+
+    private static void MainWithLinkedList()
+    {
+      var timer = new Stopwatch();
+      timer.Start();
+
+      int elfCount = 3017957;
 
       LinkedList<int> elves = new LinkedList<int>(Enumerable.Range(1, elfCount));
       var currentElf = elves.First;
