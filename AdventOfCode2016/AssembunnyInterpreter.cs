e144bafe434d1042f2337c268af19f94f100944a
Matthew Richards <github@matthewrichards.org.uk>
2016-12-23 10:21:38 +0000

    Refactor to extract Assembunny interpreter

---
+++ AdventOfCode2016/AssembunnyInterpreter.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2016
+{
+  class AssembunnyInterpreter
+  {
+    public void Execute(string input, Dictionary<char, int> registers)
+    {
+      var program = input.SplitLines().Select(line => line.Split(' ')).ToArray();
+      int instr = 0;
+
+      while (instr < program.Length)
+      {
+        int nextInstr = instr + 1;
+
+        switch (program[instr][0])
+        {
+          case "cpy":
+            var source = program[instr][1];
+            int sourceValue;
+            if (!int.TryParse(source, out sourceValue))
+            {
+              sourceValue = registers[source.Single()];
+            }
+
+            var targetRegister = program[instr][2].Single();
+            registers[targetRegister] = sourceValue;
+            break;
+
+          case "inc":
+            var registerToInc = program[instr][1].Single();
+            registers[registerToInc] = registers[registerToInc] + 1;
+            break;
+
+          case "dec":
+            var registerToDec = program[instr][1].Single();
+            registers[registerToDec] = registers[registerToDec] - 1;
+            break;
+
+          case "jnz":
+            var variableToTest = program[instr][1];
+            int valueToTest;
+            if (!int.TryParse(variableToTest, out valueToTest))
+            {
+              valueToTest = registers[variableToTest.Single()];
+            }
+
+            if (valueToTest != 0)
+            {
+              nextInstr = instr + int.Parse(program[instr][2]);
+            }
+            break;
+        }
+
+        instr = nextInstr;
+      }
+    }
+  }
+}
--- AdventOfCode2016/Day12.cs
+++ AdventOfCode2016/Day12.cs
@@ -13,62 +13,14 @@ namespace AdventOfCode2016
   {
     private static void Main12()
     {
-      var program = Input.SplitLines().Select(line => line.Split(' ')).ToArray();
+      Dictionary<char, int> registers = new Dictionary<char, int> { { 'a', 0 }, { 'b', 0 }, { 'c', 1 }, { 'd', 0 } };
 
-      Dictionary<char, int> registers = new Dictionary<char, int> {{'a', 0}, {'b', 0}, {'c', 1}, {'d', 0}};
-      int instr = 0;
-
-      while (instr < program.Length)
-      {
-        int nextInstr = instr + 1;
-
-        switch (program[instr][0])
-        {
-          case "cpy":
-            var source = program[instr][1];
-            int sourceValue;
-            if (!int.TryParse(source, out sourceValue))
-            {
-              sourceValue = registers[source.Single()];
-            }
-
-            var targetRegister = program[instr][2].Single();
-            registers[targetRegister] = sourceValue;
-            break;
-
-          case "inc":
-            var registerToInc = program[instr][1].Single();
-            registers[registerToInc] = registers[registerToInc] + 1;
-            break;
-
-          case "dec":
-            var registerToDec = program[instr][1].Single();
-            registers[registerToDec] = registers[registerToDec] - 1;
-            break;
-
-          case "jnz":
-            var variableToTest = program[instr][1];
-            int valueToTest;
-            if (!int.TryParse(variableToTest, out valueToTest))
-            {
-              valueToTest = registers[variableToTest.Single()];
-            }
-
-            if (valueToTest != 0)
-            {
-              nextInstr = instr + int.Parse(program[instr][2]);
-            }
-            break;
-        }
-
-        instr = nextInstr;
-      }
+      new AssembunnyInterpreter().Execute(Input, registers);
 
       Console.WriteLine(registers['a']);
       Console.ReadKey();
     }
 
-    
     private const string Input = @"cpy 1 a
 cpy 1 b
 cpy 26 d
